2a0f644b0f53f862c75ef09b8880318add6d2847
scooby32 <me1395@hotmail.co.uk>
2015-03-19 09:50:48 +0000

    Messed around with code in auction system. expanded switch and added an auction

--- Workspace/auction/auction/AuctionSystem.java
+++ Workspace/auction/auction/AuctionSystem.java
@@ -1,5 +1,6 @@
 package auction;
 
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 import auction.User;
@@ -8,21 +9,38 @@ public class AuctionSystem {
 
 	Scanner cin = new Scanner(System.in);
 	
+	static Auction ChocBar = new Auction(5, 10, LocalDateTime.of(2014, 7, 15, 12, 30));
+	
 	public AuctionSystem() {
 		User adminBuy = new Buyer("admin","changeme");
 		User adminSell = new Seller("admin","changeme");
 		
-		System.out.println("Please selection an option:");
-		System.out.println("1. Login");
-		System.out.println("2. Register");
+		//Auction ChocBar = new Auction(5, 10, LocalDateTime.of(2014, 7, 15, 12, 30));
 		
-		String choice;
-		choice = cin.next();
+		String choice = null;
 		
-		switch(choice){
-		case "1": //Login
-		case "2": //Register
-			break;
+		while(choice != "4"){
+			System.out.println("Please selection an option:");
+			System.out.println("1. Login");
+			System.out.println("2. Register");
+			System.out.println("3. Browse");
+			System.out.println("4. Quit");
+		
+			choice = cin.next();
+		
+			switch(choice){
+			case "1": //Login
+				LogIn();
+				break;
+			case "2": //Register
+				SetupAccount();
+				break;
+			case "3": //Browse
+				BrowseAuction();
+				break;
+			case "4": //Quit
+				System.exit(0);
+			}
 		}
 	}
 	
@@ -33,10 +51,15 @@ public class AuctionSystem {
 	public void BrowseAuction() {
 		//No return type: output to console
 		//Note: Used by PlaceBid and Browsers, don't expect input
+		System.out.println(AuctionSystem.ChocBar);
 	}
 	
 	public void SetupAccount() {
 		//No return type: runs functions and input loop
+	}
+	
+	public void LogIn() {
+		
 	}
 	
 }
